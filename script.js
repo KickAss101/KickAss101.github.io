0578757e2ec3ab2a4457e1d6f652c61b45bae388
KickAss101 <jaswanthrck@gmail.com>
2020-01-15 18:11:31 +0530

    added typing effect and font colors

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-window.addEventListener("scroll", appearOnScroll)
+const screenPosition = window.innerHeight / 1.3
 
-function appearOnScroll(){
-    let greeting = document.querySelector(".greeting")
-    let greetingPosition = greeting.getBoundingClientRect().top
-    let screenPosition = window.innerHeight / 1.3
-    if(greetingPosition < screenPosition)
-        greeting.classList.add("appear")
+window.addEventListener("scroll", () => {
+    appear(".greeting", 1, 0)
+    appear(".code", 1.8, 0)
+    appear(".echo", 0, 9)
+    animateText(".line")
+})
+
+function appear(ele, speed, delay){
+    const item = document.querySelector(ele)
+    const itemPosition = item.getBoundingClientRect().top
+    if(itemPosition < screenPosition){
+        item.style.transition = `${speed}s ${delay}s all ease-in`
+        item.classList.add("appear")
+    }
+}
+
+function animateText(ele){
+    const item = document.querySelector(ele)
+    const itemPosition = item.getBoundingClientRect().top
+    if(itemPosition < screenPosition){
+        item.style.opacity = `${1}`
+        item.classList.add("anim-typewriter")
+    }
 }
