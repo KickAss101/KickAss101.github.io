68a992bdb3100fb8b829c3863a381a33fc63175a
KickAss101 <jaswanthrck@gmail.com>
2020-01-16 14:44:50 +0530

    fixed responsive bug in typeWriter Effect with JS

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const screenPosition = window.innerHeight / 1.3
+const screenPosition = window.innerHeight / 1.5
+const text = document.querySelector("#animate")
+let i = 0
 
 window.addEventListener("scroll", () => {
     appear(".greeting", 1, 0)
-    appear(".code", 1.8, 0)
-    appear(".echo", 0, 9)
-    animateText(".line")
-})
+    appear(".code", 1, .2)
+    appear(".echo", 0, 7.8)
+    if(text.getBoundingClientRect().top < screenPosition){
+        setInterval(typeWriter, 100, "#animate")
+    }
+},)
 
 function appear(ele, speed, delay){
     const item = document.querySelector(ele)
     const itemPosition = item.getBoundingClientRect().top
     if(itemPosition < screenPosition){
-        item.style.transition = `${speed}s ${delay}s all ease-in`
+        item.style.transition = `${speed}s ${delay}s all ease-in-out`
         item.classList.add("appear")
     }
 }
 
-function animateText(ele){
-    const item = document.querySelector(ele)
-    const itemPosition = item.getBoundingClientRect().top
-    if(itemPosition < screenPosition){
-        item.style.opacity = `${1}`
-        item.classList.add("anim-typewriter")
+function typeWriter(selector){
+    let text = `echo "I'm a web developer, this is just a showcase of my projects."`
+    let ele = document.querySelector(selector)
+    if(i < text.length){
+        ele.innerHTML += text.charAt(i)
+        i++
     }
 }
